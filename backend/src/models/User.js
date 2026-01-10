f3a33f9da45903a66fc9780725e376a1bf8cf913
anandu <ananduvinod04@gmail.com>
2026-01-10 11:52:35 +0530

    fix/otp varification bug

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,38 +1,42 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema( 
-    {
+const userSchema = new mongoose.Schema(
+  {
     name: {
       type: String,
       required: true,
-      trim: true,
     },
-
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     email: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
     },
-
     password: {
       type: String,
+      required: true,
+    },
+    dob: {
+      type: Date,
       required: true,
     },
   },
   { timestamps: true }
 );
 
-//  Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// 
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
-//  Compare password during login
+// Password match method
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
